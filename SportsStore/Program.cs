d795f686a104a1087b1554093d311d06bc0bada2
mnek1t <mednikita2004@gmail.com>
2024-05-12 23:18:48 +0300

    Add data to application.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,11 +1,24 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+using SportsStore.Models.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
   
 app.MapDefaultControllerRoute();
+
+// seed the database when the application starts,
+SeedData.EnsurePopulated(app);
 
 app.Run();
